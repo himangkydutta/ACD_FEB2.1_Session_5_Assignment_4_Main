520a7c92656a116584bdfa0d7af717b7758f4093
himangkydutta <himangkydutta08@gmail.com>
2017-04-27 01:18:08 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+//Declaring and defining employee array storing five different object
+
+var multipleEmparray = ["Ankita",28,300000,"Bangalore","Karnataka",560100];
+
+
+//Displaying each object and array in console
+console.log(multipleEmparray);
+
+//or through index
+console.log(multipleEmparray[0]);
+console.log(multipleEmparray[1]);
+console.log(multipleEmparray[2]);
+console.log(multipleEmparray[3]);
+console.log(multipleEmparray[4]);
+console.log(multipleEmparray[5]);
+
